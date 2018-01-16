4cec04a569ff50da61bbde19b7237626478ac9a7
Bhuvaneshwari Rajasekar <Bhuvana.Rajasekar@siriuscom.com>
2018-01-17 00:01:16 +0530

    Add and movie detail service

--- ProjectorServer/routes/movies.js
+++ ProjectorServer/routes/movies.js
@@ -2,6 +2,8 @@ var express = require('express'),
      router = express.Router(),
      moviesController = require('../controllers/moviesController');
 
- router.get('/all', moviesController.getAllMovies);
-
+ router.get('/all', moviesController.getAllMovies)
+        .post('/add', moviesController.addNewMovie)
+        .get('/:movieName', moviesController.getMovieDetails);
+        
  module.exports = router;
